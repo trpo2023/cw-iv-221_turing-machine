9b487a0366885983cf89bb48ec118c134aedee92
TynyanovVadim <vadiktynyanov@yandex.ru>
2023-05-02 23:19:52 +0700

    Get data from console, #5

---
+++ src/libturing/procces_input_data.cpp
@@ -0,0 +1,38 @@
+#include <fstream>
+
+#include "procces_input_data.hpp"
+#include "changes_according_to_the_rules.hpp"
+
+int process_input_console(ifstream *rule, string res)
+{
+    string state = BEG_STATE;
+
+    string str, value;
+    int i = 0, next = 0;
+
+    cout << "Enter the line: ";
+    getline(cin, str);
+    while (1) {
+        value = str[i];
+        if ((next = seek_unit_in_file(rule, &state, &value)) == -2) {
+            cout << value << endl;
+            cout << state << endl;
+            return -1;
+        }
+
+        str[i] = value[0];
+        i += next;
+        if (i < 0) {
+            i = 0;
+            str.insert(str.begin(), ' ');
+        } else if (i >= (int) str.size()) {
+            str.push_back(' ');
+        }
+
+        if (state == END_STATE) {
+            break;
+        }
+    }
+    res = str;
+    return 0;
+}

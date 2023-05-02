2fad8e0bfbf81693272c2c3b2a8e9f91e840091f
TynyanovVadim <vadiktynyanov@yandex.ru>
2023-05-02 23:23:06 +0700

    Process input data, main, #6

---
+++ src/libturing/procces_input_data.hpp
@@ -0,0 +1,12 @@
+#define BEG_STATE "q_b"
+#define END_STATE "q_e"
+
+#define ERROR_FILE_NO_SPECIFIED "No file specified"
+#define ERROR_FILE_NOT_FOUND "File not found"
+#define ERROR_TOO_MANY_ARGUMENTS "Too many arguments"
+
+#define RULES_FILE_ARGUMENT 2
+#define INPUT_FILE_ARGUMENT 3
+
+int process_input_console(std::ifstream *rule);
+int process_input_file(std::ifstream *rule, std::fstream *input);

db018da89a05075ffa6114a421ca9f78b24b20b2
TynyanovVadim <vadiktynyanov@yandex.ru>
2023-05-10 09:27:56 +0700

    Add declaration functions in hpp file

--- src/libturing/file_validation_check.hpp
+++ src/libturing/file_validation_check.hpp
@@ -6,3 +6,7 @@
 #define ERROR_EXCPECTED_DELIM "expected \"->\""
 #define ERROR_EXCPECTED_SHIFT_MODE "expected shift mode (R, L or N)"
 #define ERROR_UNEXCPECTED_TOKEN "unexpected token"
+
+int check_arrow(std::string str, int *index);
+int check_shift(std::string str, int* index);
+int check_value(std::string str, int* index);

2354a34054001822d0ae1de42fa37e32a7e58227
TynyanovVadim <vadiktynyanov@yandex.ru>
2023-05-10 09:27:16 +0700

    Add test for check_shift

--- test/syntax_test.cpp
+++ test/syntax_test.cpp
@@ -24,3 +24,39 @@ CTEST(syntax_suite, check_arrow_false)
     ASSERT_TRUE(check_arrow(str2, &index2));
     ASSERT_EQUAL(0, index2);
 }
+
+CTEST(syntax_suite, check_shift_true)
+{
+    std::string s1 = "  R  ";
+    std::string s2 = "  r  ";
+    std::string s3 = "  L  ";
+    std::string s4 = "  l  ";
+    std::string s5 = "  N  ";
+    std::string s6 = "  n  ";
+
+    int index = 0;
+    ASSERT_FALSE(check_shift(s1, &index));
+
+    index = 0;
+    ASSERT_FALSE(check_shift(s2, &index));
+
+    index = 0;
+    ASSERT_FALSE(check_shift(s3, &index));
+
+    index = 0;
+    ASSERT_FALSE(check_shift(s4, &index));
+
+    index = 0;
+    ASSERT_FALSE(check_shift(s5, &index));
+
+    index = 0;
+    ASSERT_FALSE(check_shift(s6, &index));   
+}
+
+CTEST(syntax_suite, check_shift_false)
+{
+    std::string s1 = "A";
+    int index = 0;
+
+    ASSERT_TRUE(check_shift(s1, &index));  
+}

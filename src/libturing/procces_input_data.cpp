7b34907091730566e1f5e2ddd3b4879865a6f886
TynyanovVadim <vadiktynyanov@yandex.ru>
2023-05-14 10:34:18 +0700

    Add common fuction process_string

--- src/libturing/procces_input_data.cpp
+++ src/libturing/procces_input_data.cpp
@@ -3,22 +3,20 @@
 #include "changes_according_to_the_rules.hpp"
 #include "procces_input_data.hpp"
 
-int process_input_file(ifstream* rule, ifstream* input, string* res)
+int process_string(ifstream* rule, string* data, string* res)
 {
     string state = BEG_STATE;
-    string str, value;
+    string str = *data, value;
     int i = 0, next = 0;
-
-    getline(*input, str);
+    
     while (1) {
         value = str[i];
         if ((next = seek_unit_in_file(rule, &state, &value)) == -2) {
-            cout << value << endl;
-            cout << state << endl;
-            return -1;
+            break;
         }
 
-        str[i] = value[0];
+        str.erase(i, 1);
+        str.insert(i, value);
         i += next;
         if (i < 0) {
             i = 0;
@@ -36,36 +34,19 @@ int process_input_file(ifstream* rule, ifstream* input, string* res)
     return 0;
 }
 
+int process_input_file(ifstream* rule, ifstream* input, string* res)
+{
+    string str;
+
+    getline(*input, str);
+    return process_string(rule, &str, res);
+}
+
 int process_input_console(ifstream* rule, string* res)
 {
-    string state = BEG_STATE;
-
-    string str, value;
-    int i = 0, next = 0;
-
+    string str;
     cout << "Enter the line: ";
     getline(cin, str);
-    while (1) {
-        value = str[i];
-        if ((next = seek_unit_in_file(rule, &state, &value)) == -2) {
-            cout << value << endl;
-            cout << state << endl;
-            return -1;
-        }
-
-        str[i] = value[0];
-        i += next;
-        if (i < 0) {
-            i = 0;
-            str.insert(str.begin(), ' ');
-        } else if (i >= (int)str.size()) {
-            str.push_back(' ');
-        }
-
-        if (state == END_STATE) {
-            break;
-        }
-    }
-    *res = str;
-    return 0;
+    
+    return process_string(rule, &str, res);
 }
